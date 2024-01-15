702308c0b9d9a1268e2371aae1cb8546d63d889f
Sharath_Hegde <hegde.sharath@tum.de>
2024-01-15 11:36:50 +0100

    Calculate average moment - plotting error

--- src/FLOWUnsteady_monitors.jl
+++ src/FLOWUnsteady_monitors.jl
@@ -354,6 +354,7 @@ function generate_monitor_rotors( rotors::Array{vlm.Rotor, 1},
         fig_moment.set_xlabel(t_lbl)
         fig_moment.set_ylabel(L"Pitching moment $M$ (Nm)")
         fig_moment.plot([t_scaled], [pitching_moment_avg], "$(stls[j])", alpha=alpha, color=clr)
+        push!(out_figs, fig_moment)
 
        # End of modified part
 
